146b602a2db17f758c399170d17487af2a304403
Asen Sanjula <asensanjula@gmail.com>
2022-04-13 12:05:12 +0530

    feat: args template - reduces the boilerplate code

--- src/Components/Button/Button.stories.js
+++ src/Components/Button/Button.stories.js
@@ -5,9 +5,34 @@ import React from "react";
 export default {
     title: 'Button',
     component: Button,
+    args: {
+        children: 'Button'
+    }
 }
 
 export const Primary = () => <Button variant='primary'>Primary</Button>
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>
 export const Success = () => <Button variant='success'>Success</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+//args
+
+const Template = args => <Button {...args}/>
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+    variant:'primary',
+    children: 'Primary Args' // override the component level story args
+}
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+    ...PrimaryA.args,
+    children:'Long Primary A'
+}
+
+export const SecondaryA = Template.bind({});
+SecondaryA.args = {
+    variant:'secondary',
+    children: 'Secondary Args'
+}
