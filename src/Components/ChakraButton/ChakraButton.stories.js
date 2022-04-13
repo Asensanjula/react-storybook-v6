660e936799ecdcfa8fe2858625ca6cf49045e655
Asen Sanjula <asensanjula@gmail.com>
2022-04-13 21:54:14 +0530

    feat: action addon

--- src/Components/ChakraButton/ChakraButton.stories.js
+++ src/Components/ChakraButton/ChakraButton.stories.js
@@ -4,8 +4,20 @@ import React from "react";
 
 export default {
     title:'Chakra/Button',
-    component: Button
+    component: Button,
+    argTypes: {
+        onClick: { action: 'clicked' }
+        },
 }
 
-export const Success = () => <Button variantColor='green'>Success</Button>
-export const Danger = () => <Button variantColor='red'>Success</Button>
+const Template = args => <Button {...args}/>
+
+
+export const Success = Template.bind({});
+Success.args = {
+    variantColor:'green',
+    children: 'Success',
+}
+
+// export const Success = () => <Button variantColor='green'>Success</Button>
+// export const Danger = () => <Button variantColor='red'>Success</Button>
